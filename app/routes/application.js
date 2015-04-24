c11771df51e2fe624e80e2b1fecda6ba056fd298
Deepaligandhi <deepali.gandhi@gmail.com>
2015-04-24 10:21:24 -0700

    Session authentication added

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,12 +3,31 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   authentication: Ember.inject.service('authentication'),
 
+  beforeModel: function() {
+    var route = this;
+    var promise = this.store.find('user', {isAuthenticated: true});
+    return promise.then(function(users) {
+      if (users && users.get('length') > 0) {
+        var user = users.get('firstObject');
+        route.set('session.user', user);
+        console.log('session.user');
+      }
+      return users;
+    });
+  },
+
   actions: {
     logout: function() {
       this.get('authentication').set('authenticatedUser', null);
-      this.store.unloadAll('user');
-      this.store.unloadAll('post');
-      this.transitionTo('home.login');
+
+      var self = this;
+      Ember.$.get('/api/logout')
+       .done(function(){
+         self.get('session').set('user', null);
+         self.store.unloadAll('post');
+         self.store.unloadAll('user');
+         self.transitionTo('home.login');
+        });
     },
   }
 });
